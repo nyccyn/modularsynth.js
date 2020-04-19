92b5dc05c4c91eb3b4fb2f2c75e56ed85417703a
nyccyn <nyccyn@gmail.com>
2020-04-19 12:29:44 +0300

    Updates

---
+++ precache-manifest.c38b5954b96d6742d6eacbf3711d0d9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da51eaa180079c2a48db5b661c98fba",
+    "url": "/modularsynth.js/index.html"
+  },
+  {
+    "revision": "403d6cd68ae5013fe31e",
+    "url": "/modularsynth.js/static/css/main.73f61043.chunk.css"
+  },
+  {
+    "revision": "545490fa3fcda366f48c",
+    "url": "/modularsynth.js/static/js/2.9affce8d.chunk.js"
+  },
+  {
+    "revision": "403d6cd68ae5013fe31e",
+    "url": "/modularsynth.js/static/js/main.277deb8a.chunk.js"
+  },
+  {
+    "revision": "3c33c5eca6c908167a5d",
+    "url": "/modularsynth.js/static/js/runtime~main.1deac282.js"
+  },
+  {
+    "revision": "23f417df4a1693d42ada12d67a43df25",
+    "url": "/modularsynth.js/static/media/knob.23f417df.svg"
+  },
+  {
+    "revision": "1b33f6c1c64e124981e0dd9595dd9389",
+    "url": "/modularsynth.js/static/media/port.1b33f6c1.svg"
+  },
+  {
+    "revision": "f9689bc28c5ac0afef6db2bb8b84a5f7",
+    "url": "/modularsynth.js/static/media/pulse.f9689bc2.svg"
+  },
+  {
+    "revision": "cd50a535e7fb779c88ffcc1d4155973f",
+    "url": "/modularsynth.js/static/media/rack_bg.cd50a535.svg"
+  },
+  {
+    "revision": "de5110173a4103017518856888a3c689",
+    "url": "/modularsynth.js/static/media/sawtooth.de511017.svg"
+  },
+  {
+    "revision": "2c2805d6c6f43626dafa83f63d25f573",
+    "url": "/modularsynth.js/static/media/screw_header.2c2805d6.svg"
+  },
+  {
+    "revision": "e19941034d78b5b838e32d4d78cab2a4",
+    "url": "/modularsynth.js/static/media/sine.e1994103.svg"
+  },
+  {
+    "revision": "87d201488a6d7693a98cfda19266b657",
+    "url": "/modularsynth.js/static/media/triangle.87d20148.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modularsynth.js/precache-manifest.4afca152031d538546be8b05f00e33b5.js"
+  "/modularsynth.js/precache-manifest.c38b5954b96d6742d6eacbf3711d0d9b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/modularsynth.js/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modularsynth.js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
